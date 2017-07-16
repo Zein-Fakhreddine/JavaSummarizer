f9bdc77f4ff5ef31ff50203f438b051ed39c2955
ZeinF <1Astronomy>
2017-07-15 22:55:31 -0500

    Improved the way in which the article is found. Fixed a bug in which the paragraphs where not in the right order

--- src/com/zeinfakhreddine/summarizer/Paragraph.kt
+++ src/com/zeinfakhreddine/summarizer/Paragraph.kt
@@ -1,24 +1,24 @@
 package com.zeinfakhreddine.summarizer
 
-internal class Paragraph(val sentences: ArrayList<String>) {
+internal class Paragraph(val sentences: ArrayList<String>, val index: Int) {
 
     val words: ArrayList<Word> = ArrayList()
     private val _totalWeight: Int? = null
     val totalWeight: Int
-    get() {
-        if(_totalWeight == null)
-            return calculateTotalWeight()
+        get() {
+            if (_totalWeight == null)
+                return calculateTotalWeight()
 
-        return _totalWeight
-    }
+            return _totalWeight
+        }
 
     /**
      * Splits the sentences into words and adds it to the array list
      */
     init {
         sentences.forEach {
-            it.split(" ").forEach {
-                words.add(Word(it, 0))
+            it.split(" ").mapTo(words) {
+                Word(it, 0)
             }
         }
     }
@@ -26,7 +26,7 @@ internal class Paragraph(val sentences: ArrayList<String>) {
     /**
      *Calculates total weight of each word in paragraph
      */
-    private fun calculateTotalWeight() : Int {
+    private fun calculateTotalWeight(): Int {
         var total = 0
         words.forEach { total += it.weight }
         return total
@@ -36,9 +36,7 @@ internal class Paragraph(val sentences: ArrayList<String>) {
      * Combines all the words
      */
     override fun toString(): String {
-        val wordArray = words.map{
-            it.word
-        }
+        val wordArray = words.map { it.word }
 
         return "<p> ${wordArray.joinToString(" ")} </p>"
     }
@@ -53,6 +51,17 @@ internal class Paragraph(val sentences: ArrayList<String>) {
                 return -1
             else
                 return 1
+        }
+    }
+
+    class IndexComparator : Comparator<Paragraph>{
+        override fun compare(s1: Paragraph, s2: Paragraph): Int {
+            if (s1.index == s2.index)
+                return 0
+            else if (s1.index > s2.index)
+                return 1
+            else
+                return -1
         }
     }
 }
--- src/com/zeinfakhreddine/summarizer/Summarizer.kt
+++ src/com/zeinfakhreddine/summarizer/Summarizer.kt
@@ -8,7 +8,8 @@ import org.jsoup.select.Elements
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 3, var title: String? = null) {
+class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 4, var title: String? = null) {
+
     /**
      *Uses Levenshtein distance to calculate the difference between a given String (this) and a param
      */
@@ -48,21 +49,20 @@ class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 3, var title: Stri
      * Unfluffs the document to just get paragraphs that have to do with the article
      * Adds the gotten paragraphs to a global array list of paragraphs
      */
-    fun unfluff(doc: Document){
-        val divs: Elements = doc.select("div")
+    private fun unfluff(doc: Document) {
+        val divs: Elements = doc.select("*")
         var mainDiv: Element? = null
         var amount = 0
 
         divs.forEach {
-            //Only get the first level of children for each div element
             val detachedDivChildren = Elements()
+
             it.children().mapTo(detachedDivChildren) {
                 Element(Tag.valueOf(it.tagName()),
                         it.baseUri(), it.attributes().clone())
             }
 
             val amountOfP = detachedDivChildren.select("p").size
-
             if (amountOfP > amount) {
                 amount = amountOfP
                 mainDiv = it
@@ -70,29 +70,33 @@ class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 3, var title: Stri
         }
 
         if(mainDiv == null){
-            summary = "Error not able to find paragraphs"
+            summary = "Error not able to find articles"
             return
         }
+
 
         mainDiv?.select("p")?.forEach { element ->
             //Makes sure the paragraph has to due with the article and not something else. e.x: capture for picture
             var badP = false
-            element.children().forEach {
-                if (it.select("span").size > 0)
-                    badP = true
+            if (element.parent() != mainDiv)
+                badP = true
+            else {
+                element.children().forEach {
+                    if (it.select("span").size > 0)
+                        badP = true
+                }
             }
-
             if (!badP) {
                 val sentences = element.text().split("(?<![DSJ]r|Mrs?)[.?!](?!\\S)").joinToString("").split("\n")
 
-                paragraphs.add(Paragraph(sentences = ArrayList(sentences)))
+                paragraphs.add(Paragraph(sentences = ArrayList(sentences), index = paragraphs.size))
             }
         }
 
-        if(paragraphs.size >= NUMBER_OF_PARAGRAPHS + 1)
+        if (paragraphs.size >= NUMBER_OF_PARAGRAPHS + 1)
             addWeights()
-        else{
-            summary = "Error not able to find paragraphs"
+        else {
+            summary = "Error not able to find enough paragraphs"
             return
         }
     }
@@ -101,7 +105,7 @@ class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 3, var title: Stri
      * Adds weight to each word in each paragraph
      * the weight for each word is based on  the sum of its distance* to each word in the title
      */
-    fun addWeights() {
+    private fun addWeights() {
         val allWords = ArrayList<Paragraph.Word>()
 
         paragraphs.forEach { allWords.addAll(it.words) }
@@ -121,19 +125,22 @@ class Summarizer(url: String, var NUMBER_OF_PARAGRAPHS: Int = 3, var title: Stri
      * it adds the first article of the paragraph (for introduction)
      * then adds the top 5 weighted articles
      */
-    fun loadSummary() {
-        if(summary != null)
+    private fun loadSummary() {
+        if (summary != null)
             return
         summary = ""
         summary += paragraphs[0].toString()
         paragraphs.removeAt(0)
+
         Collections.sort(paragraphs, Paragraph.WeightComparator())
-        val itr = paragraphs.iterator()
-        var i = 0
-        while (itr.hasNext() && i != NUMBER_OF_PARAGRAPHS) {
-            val paragraphText = itr.next().toString()
-            summary += paragraphText
-            i++
+        paragraphs.removeIf {
+            paragraphs.indexOf(it) >= NUMBER_OF_PARAGRAPHS - 1
+        }
+
+        Collections.sort(paragraphs, Paragraph.IndexComparator())
+
+        paragraphs.forEach {
+            summary += it.toString()
         }
     }
 }
